e4e772696c32109820197c9c1f90da13e642deae
Daniel Murphy <48087364+dxnnymurphy@users.noreply.github.com>
2022-03-09 16:53:17 +0000

    filtering added
    multiple checkbox filters currently not working together

---
+++ mysite/journal_app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+class NewJournalForm(forms.Form):
+    title = forms.CharField(label='title', required = True)
+    url = forms.CharField(label= 'url', required = False)
+    software = forms.CharField(label='software', required = False)
+    notes = forms.CharField(label='notes', required=False)
+
+
+class FilterForm(forms.Form):
+    term = forms.CharField(label='term', required = False)
+    python = forms.BooleanField(required=False)
+    django = forms.BooleanField(required=False)
+    html = forms.BooleanField(required=False)
+    css = forms.BooleanField(required=False)
+
+    
